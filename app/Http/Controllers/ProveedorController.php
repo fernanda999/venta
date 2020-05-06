8d8db78f5ef92627d268a82ca95d6bae90566cb2
fernanda999 <ninaf610@gmail.com>
2020-05-06 01:18:39 -0400

    version 1

---
+++ app/Http/Controllers/ProveedorController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace sisVentas\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use sisVentas\Http\Requests;
+use sisVentas\Persona;
+use Illuminate\Support\Facades\redirect;
+use sisVentas\Http\Requests\PersonaFormRequest;
+use DB;
+
+class ProveedorController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    public function index(Request $request)
+    {
+        if ($request)
+        {
+            $query=trim($request->get('searchText'));
+            $personas=DB::table('persona')
+            ->where('nombre','LIKE','%'.$query.'%')
+            ->where ('tipo_persona','=','Proveedor')
+            ->orwhere('num_documento','LIKE','%'.$query.'%')
+            ->where ('tipo_persona','=','Proveedor')
+            ->orderBy('idpersona','desc')
+            ->paginate(7);
+            return view('compras.proveedor.index',["personas"=>$personas,"searchText"=>$query]);
+        }
+    }
+
+    public function create()
+    {
+        return view('compras.proveedor.create');
+    }
+
+    public function store(PersonaFormRequest $request)
+    {
+        $persona=new Persona;
+        $persona->tipo_persona='Proveedor';
+        $persona->nombre=$request->get('nombre');
+        $persona->tipo_documento=$request->get('tipo_documento');
+        $persona->num_documento=$request->get('num_documento');
+        $persona->direccion=$request->get('direccion');
+        $persona->telefono=$request->get('telefono');
+        $persona->email=$request->get('email');
+        $persona->save();
+        //$persona=new Persona;
+       // dd($request->all());
+       // Persona::create($request->all());
+        //$persona->save();
+        return Redirect::to('compras/proveedor');
+    }
+
+
+    public function show($id)
+    {
+        return view("compras.proveedor.show",["persona"=>Persona::findOrfail($id)]);
+    }
+
+    public function edit($id)
+    {
+        return view("compras.proveedor.edit",["persona"=>Persona::findOrfail($id)]);
+    }
+
+    public function update(PersonaFormRequest $request, $id)
+    {
+        $persona=Persona::findOrfail($id);
+        
+        $persona->nombre=$request->get('nombre');
+        $persona->tipo_documento=$request->get('tipo_documento');
+        $persona->num_documento=$request->get('num_documento');
+        $persona->direccion=$request->get('direccion');
+        $persona->telefono=$request->get('telefono');
+        $persona->email=$request->get('email');
+
+        $persona->update();
+
+        return Redirect::to('compras/proveedor');
+    }
+
+    public function destroy($id)
+    {
+        $persona=Persona::findOrfail($id);
+        $persona->tipo_persona='Inactivo';
+        $persona->update();
+
+        return redirect::to('compras/proveedor');
+    }
+}
